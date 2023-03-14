54dcdaeb14528ddb7674be00470855a4988e3212
StepanGusev <lazar.pwn@gmail.com>
2023-03-15 01:06:45 +0300

    добавил CSRF  в login, post запрос в logout.

--- src/main/java/ru/kata/spring/boot_security/demo/config/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/config/WebSecurityConfig.java
@@ -35,7 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
